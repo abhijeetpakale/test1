1bcf7a1b928a560536a701225c0f50d1366a39ae
Abhijeet <Abhijeet@DESKTOP-VK4PF7E>
2022-10-30 13:19:08 +0530

    first commit 1

---
+++ src/basic/demo1.java
@@ -0,0 +1,31 @@
+package basic;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hiii");
+		m1();
+	}
+	
+	
+	
+	public static void  m1() {
+		
+		
+		System.out.println("abhijeet pakale");
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
